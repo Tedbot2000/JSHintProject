571b4d9383a3795a1915a06d6fb3d19dd472e29e
Edward Aylmer <mail@tedaylmer.co.uk>
2024-06-27 12:56:55 +0000

    API Key removed for privacy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "ABX0qKgEm2TTGqec6pzBxIe40CU";
+const API_KEY = ""; // Key removed for Privacy
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
